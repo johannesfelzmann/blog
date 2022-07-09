beb11751215d42e9ca15f3fba5961f089b031443
Johannes Felzmann <e11912368@student.tuwien.ac.at>
2022-07-09 16:56:15 +0200

    added about + welcome

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
 export class AppComponent{
   
   title = 'Johannes Felzmann`s Blog';
+
+  constructor(public router: Router) {
+
+  }
 }
 
